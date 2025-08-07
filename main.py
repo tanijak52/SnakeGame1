b84f63e98f024cccd3ca2be328e5083383ed9ce8
tanijak52 <tatuana7887yy@gmail.com>
2025-08-07 13:26:12 +0200

    Game share

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ import sys
 
 pygame.init()
 
-win_width = 600
-win_height = 600
-
-
+win_width = 720
+win_height = 480
 window = display.set_mode((win_width, win_height))
 display.set_caption("Snake")
 
@@ -20,11 +18,12 @@ button_image = transform.scale(image.load("button2.png"), (300, 80))
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = Color('gray15')
-LIGHT_BLUE = Color('green')
+LIGHT_BLUE = Color('while')
 
 
 font = pygame.font.SysFont("Arial", 40)
 small_font = pygame.font.Font(None, 30)
+font_gameover = pygame.font.SysFont("Arial", 45)
 
 menu_items = ["Play", "Quit"]
 menu_functions = ["start_game", "quit"]
@@ -42,13 +41,53 @@ input_color_active = LIGHT_BLUE
 input_color_inactive = GRAY
 input_color = input_color_inactive
 
-def start_game(nickname):
-    print(f"Гра почалася для гравця: {nickname}")
-
-
 def quit_game():
     pygame.quit()
     sys.exit()
+
+def start_game(nickname):
+    WSize = (720, 480)
+    win_game = display.set_mode(WSize)
+
+    TSide = 30
+    MSize = WSize[0] // TSide, WSize[1] // TSide
+
+    start_pos = MSize[0] // 2, MSize[1] // 2
+    snake = [start_game]
+    avile = True
+
+    direction = 0
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    apple = randint(0, MSize[0] - 1), randint(0, MSize[1] - 1)
+
+    FPS = 4
+    clock = time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+        win_game.fill("black")
+
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+            quit_game()
+        if e.type == KEYDOWN:
+            if alive:
+                if e.key == K_d and direction != 2:
+                    direction = 0
+                if e.key == K_s and direction != 3:
+                    direction = 1
+                if e.key == K_a and direction != 0:
+                    direction = 2
+                if e.key == K_w and direction != 1:
+                    direction = 3
+            else:
+                if e.key == K_SPACE:
+                    return
+                
+
 
 menu_actions = {
     "start_game": lambda: start_game(nickname),
